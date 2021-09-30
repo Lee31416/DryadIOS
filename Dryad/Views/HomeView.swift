e2d868aedd9a177901b2405d492b04e20867e5ef
Tommy-Lee <tom_pig9@hotmail.com>
2021-09-30 07:58:43 -0400

    [DY-012] Didn't do much nothing is working

--- Dryad/Views/HomeView.swift
+++ Dryad/Views/HomeView.swift
@@ -2,8 +2,59 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("test")
-    }
+            VStack (spacing: 10) {
+
+                Text("Bluetooth Devices")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                List() {
+                    Text("placeholder 1")
+                    Text("placeholder 2")
+                }.frame(height: 300)
+
+                Spacer()
+
+                Text("STATUS")
+                    .font(.headline)
+
+                // Status goes here
+                Text("Bluetooth status here")
+                    .foregroundColor(.red)
+
+                Spacer()
+
+                HStack {
+                    VStack (spacing: 10) {
+                        Button(action: {
+                            print("Start Scanning")
+                        }) {
+                            Text("Start Scanning")
+                        }
+                        Button(action: {
+                            print("Stop Scanning")
+                        }) {
+                            Text("Stop Scanning")
+                        }
+                    }.padding()
+
+                    Spacer()
+
+                    VStack (spacing: 10) {
+                        Button(action: {
+                            print("Start Advertising")
+                        }) {
+                            Text("Start Advertising")
+                        }
+                        Button(action: {
+                            print("Stop Advertising")
+                        }) {
+                            Text("Stop Advertising")
+                        }
+                    }.padding()
+                }
+                Spacer()
+            }
+        }
 }
 
 struct HomeView_Previews: PreviewProvider {
